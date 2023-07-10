bd86f9287073aca62aa7c685dd61423beb0d4b72
Lammyy5 <reliford2010@gmail.com>
2023-07-09 19:16:19 -0700

    Added changes to Marker location

---
+++ index.js
@@ -0,0 +1,43 @@
+// Set map 
+function initialize(){
+    var mapOptions = {
+        // Zoom of map on start
+        zoom: 10,
+        // Initial center cordinates on start (NewYork)
+        center: new google.maps.LatLng(40.7128, -74.0060),
+        // Type of map (ROADMAP, SATELLITE, HYBRID, TERRAIN)
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        //Minimum zoom of map
+        minZoom: 2
+
+    }
+
+    //Create a new map instance using provided options
+    var map = new google.maps.Map(document.getElementById
+        ('map'), mapOptions);
+        // Create an info window to display location info
+        var infoWindow = new google.maps.InfoWindow();
+
+        // Create a marker for example : California, Riverside
+        var mmarker = new google.maps.Marker({
+            // Cordinates for California, Riverside
+            position: new google.maps.LatLng(33.953350, -117.396156),
+            // Attach the marker
+            map: map,
+            // Tooltip on hover 
+            title: 'California, Riverside'
+        });
+        // Add click event listener for marker
+        marker.addListener('click', function () {
+            infoWindow.setContent(marker.title);
+            infoWindow.open(map, marker);
+        });
+        
+        //Adjust map center when the window is resized 
+        google.maps.event.addDomListener(window, "resize", function (){
+            map.setCenter(mapoptions.center);
+        });
+}
+
+//Initialize the map when window loading finished
+google.maps.event.addDomListener(window, 'load', initialize);
